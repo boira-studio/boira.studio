0e870aeb5db3267b2addae12b8bca3b7c995ec21
Quim Zudaire Prieto <quimzudaire@gmail.com>
2020-10-11 12:02:37 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
-import { I18N } from './config'
+import {
+  I18N
+} from './config'
 
 export default {
   target: 'static',
@@ -7,8 +9,8 @@ export default {
     base: '/boira.studio/',
   },
 
-   /* Layout Transitions */
-   layoutTransition: {
+  /* Layout Transitions */
+  layoutTransition: {
     name: "layout",
     mode: ""
   },
@@ -22,40 +24,45 @@ export default {
    */
   head: {
     title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
       {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
       {
         rel: 'stylesheet',
-        href:
-        'https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&display=swap'
-        }
+        href: 'https://fonts.googleapis.com/css2?family=Amiri:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+      }
     ]
   },
   /*
    ** Customize the progress bar color
    */
-  loading: { color: '#fff' },
-      /*
-    ** Global CSS
-    */
-   css: [
+  loading: {
+    color: '#fff'
+  },
+  /*
+   ** Global CSS
+   */
+  css: [
     '@/assets/scss/main.scss',
     '@/assets/scss/projectes.scss',
     '@/assets/scss/efectes.scss',
     '@/static/font/stylesheet.css',
-
-    
-
-],
+  ],
   modules: [
     ['nuxt-i18n', I18N],
     ['@nuxt/content'],
@@ -63,10 +70,10 @@ export default {
 
 
   ],
-  plugins: [
-    { src: '~/plugins/version' }
-  ],
+  plugins: [{
+    src: '~/plugins/version'
+  }],
 
-  
+
 
 }
